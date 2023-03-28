0bb2c2fbb246142110607832cac7f020caaf5b32
unknown <xhorak2@ad.fi.muni.cz>
2023-03-28 11:07:00 +0200

    fix exec

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,9 +25,12 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
- 
+    if urllib_version == '3':
+        import urllib3 as urllib
+    else:
+        import urllib
+
+    # Fetch and print the requested URL 
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
